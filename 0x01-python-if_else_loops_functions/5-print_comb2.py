448350f889669804ea171ab6813775d72f2214a6
muhajer <muhajeribrahim4@gmail.com>
2024-03-13 19:31:37 -0700

    5-print_comb2.py up

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i, j in range(0, 10):
-        num = i + j
-        sa = ","
-        if num == 18:
-            sa = " "
-        print("{}{}{} ".format(i, j, sa), end="")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end="")
+print('99')
