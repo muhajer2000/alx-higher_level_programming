267a149e1812fbe3e8177cb4191b344201db41b0
muhajer <muhajeribrahim4@gmail.com>
2024-03-16 16:15:54 -0700

    6-print_comb3.py task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for i in range(0, 10):
             continue
         if i + j == 17:
             continue
-        print(f"{i}{j}, ", end="")
+        print("{}{}, ".format(i, j), end="")
 print("89")
