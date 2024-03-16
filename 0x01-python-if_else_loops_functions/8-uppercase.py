c91299aaea6ee42aaa7853d0482ecf028c090c1a
muhajer <muhajeribrahim4@gmail.com>
2024-03-16 16:55:03 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for str1 in str:
+        if ord(str1) >= 97 and ord(str1) <= 122:
+            print("{}".format(chr(ord(str1) -  32)), end= "")
+        else:
+            print("{}".format(str1))
