c48288bed94e45a65be21e27a3599d6c8e631ec3
muhajer <muhajeribrahim4@gmail.com>
2024-03-16 18:14:09 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0:
+            n = "Fizz"
+        elif n % 5 == 0:
+            n = "Buzz"
+        elif n % 8 == 0:
+            n = "FizzBuzz"
+        print("{} ".format(n), end="")
