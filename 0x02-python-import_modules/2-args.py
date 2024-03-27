75f95b55cba45535d0379fc2f18aea1b485ab139
muhajer <muhajeribrahim4@gmail.com>
2024-03-26 17:21:56 -0700

    2-args.py task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    lists = sys.argv
-    length = len(sys.argv)
+    lists = sys.argv[1:]
+    length = len(sys.argv[1:])
 
     if length == 0:
         print("{} argument.".format(length))
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     else:
         print("{} argument:".format(length))
         for list in lists:
-            print("{} {}".format(lists.index(list), list))
+            print("{}: {}".format(lists.index(list) + 1, list))
