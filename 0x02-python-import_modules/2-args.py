f0b611fb178f1ef7914f876d7d8968859cbe9830
muhajer <muhajeribrahim4@gmail.com>
2024-03-26 17:29:22 -0700

    2-args.py updata

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     length = len(sys.argv[1:])
 
     if length == 0:
-        print("{} argument.".format(length))
+        print("{} argumentis".format(length))
     elif length == 1:
         print("{} argument:".format(length))
         print("{}: {}".format(length, lists[0]))
     else:
-        print("{} argument:".format(length))
+        print("{} arguments:".format(length))
         for list in lists:
             print("{}: {}".format(lists.index(list) + 1, list))
