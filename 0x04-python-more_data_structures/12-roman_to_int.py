2f42cd885225ee2dff7b4db99b52aae261452f2d
muhajer <muhajeribrahim4@gmail.com>
2024-05-01 09:30:49 -0700

    12-roman_to_int.py task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,6 @@ def subtract(list_ro):
         if max_list > num:
             sub += num
     return (max_list - sub)
-
 def roman_to_int(roman_string):
     if not roman_string:
         return (0)
@@ -14,7 +13,7 @@ def roman_to_int(roman_string):
         return (0)
     ro_num = {'I': 1,
               'V': 5,
-              'X' : 10,
+              'X': 10,
               'L': 50,
               'C': 100,
               'D': 500,
@@ -27,7 +26,7 @@ def roman_to_int(roman_string):
 
     for ch in roman_string:
         for r_num in list_key:
-            if r_num  == ch:
+            if r_num == ch:
                 if ro_num.get(ch) <= last_ro:
                     num += subtract(list_ro)
                     list_ro = [ro_num.get(ch)]
