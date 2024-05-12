7cdd2a0de9194233304e0aa542bfb35d35d5ac1e
muhajer <muhajeribrahim4@gmail.com>
2024-05-12 01:16:43 -0700

    1-safe_print_integer.py mod

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is not str(value):
+        if value is not str():
             print("{:d}".format(value))
         return (True)
     except ValueError:
