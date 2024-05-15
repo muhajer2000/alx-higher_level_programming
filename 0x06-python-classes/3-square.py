a95b235ff122bd7810849aa3f499b6a2a17c5406
muhajer <muhajeribrahim4@gmail.com>
2024-05-15 03:10:31 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,12 @@
+class Square:
+    """define """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        area = self.__size ** 2
+        return(area)
