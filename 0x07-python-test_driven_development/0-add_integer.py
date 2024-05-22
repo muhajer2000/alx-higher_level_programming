73115b928cd6886ce9609d9e9cf3e063f8c58e77
muhajer <muhajeribrahim4@gmail.com>
2024-05-22 09:20:32 -0700

    0-add_integer.py up

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" module for add two intgers"""
+
 def add_integer(a, b=98):
     """" add_intger : function add two intgers
 
@@ -21,8 +23,8 @@ def add_integer(a, b=98):
         a = int(a)
         b = int(b)
         return (a + b)
+
+
 if __name__ == "__main__":
     import doctest
-
-
     doctest.testfile("tests/0-add_integer.txt")
