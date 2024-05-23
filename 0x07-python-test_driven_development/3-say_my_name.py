86fea555ff094a8a55f4da8bf9582076b2c50c2f
muhajer <muhajeribrahim4@gmail.com>
2024-05-23 03:30:36 -0700

    3-say_my_name.py task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,18 +7,19 @@ def say_my_name(first_name, last_name=""):
     Arg:
         first_name: first name
         lasr_name: last name
-    return 
-        print first and last name 
+    return:
+        print first and last name
     Exceptions:
         typeerror: must be string
         typeError : mustbestring
-    
+
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
+
 
 if __name__ == "__main__":
     import doctest
