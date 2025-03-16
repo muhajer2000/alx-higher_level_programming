c846fe3f3c70252c2cdf4859407bf8daf0e3d84b
root <root@DESKTOP-9J4L3HF.localdomain>
2025-03-16 05:54:07 -0700

    upate task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,43 +5,35 @@ class Rectangle:
     """" create class Rectangle that accept the width of rectangler """
     
     def __init__(self, width = 0, height = 0):
-        
-        self.__height = height
-        self.__width = width
+        '''constator method'''
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
         """RETUREN FILE FROM THE WIETH FUNCTION , IT IS PROPERTY"""
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ setter method with name vildtion """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     @property
     def height(self):
         """RETURN FILE FROM THE FUNCTION , IT IS PROPERTY"""
         return self.__height
     
+    @width.setter
+    def width(self, value):
+        """ setter method with name vildtion """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     @height.setter
     def height(self, value):
         """THIS IS SETTER FUNCTION TO SET FROM THE PROPERTY"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
