cd985f8910a1200e05ce30b0e8f3ff733bd9b1f6
muhajer2000 <muhajeribrahim4@gmail.com>
2025-04-29 03:17:02 -0700

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const myVar = "C is fun";
+console.log(myVar);
+let lang = "Python is cool";
+console.log(lang);
+let java = "JavaScript is amazing";
+console.log(java);
